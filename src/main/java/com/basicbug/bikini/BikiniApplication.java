23badda332a3aadcc268d768b902f66a94f94975
qwebnm7788 <qwebnm7788@naver.com>
2020-11-23 22:24:55 +0900

    Initial commit
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

---
+++ src/main/java/com/basicbug/bikini/BikiniApplication.java
@@ -0,0 +1,13 @@
+package com.basicbug.bikini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basicbug/bikini/BikiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basicbug.bikini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
