790cb3d3556248a81fe997628b0b6251589637da
qwebnm7788 <qwebnm7788@naver.com>
2020-12-08 22:06:44 +0900

    Add FeedController v1/feed
    - Return single dummy feed response in /v1/feed/dummy
    
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

---
+++ src/main/java/com/basicbug/bikini/entity/FeedResponseDto.java
@@ -0,0 +1,19 @@
+package com.basicbug.bikini.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class FeedResponseDto {
+    private Integer feedNumOfUser;
+    private String userId;
+    private String content;
+    private String imageUrl;
+    private String profileImageUrl;
+    private Integer countOfGroupFeed;
+}
