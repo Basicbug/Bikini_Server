070890200f29527d9a17a330fc5ab4b151c6d4b1
qwebnm7788 <qwebnm7788@naver.com>
2021-04-25 21:40:28 +0900

    Changed name of /v1/health to /v1/common
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

--- src/main/java/com/basicbug/bikini/controller/common/HealthController.java
+++ src/main/java/com/basicbug/bikini/controller/common/HealthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/v1/health")
+@RequestMapping("/v1/common")
 @RequiredArgsConstructor
 public class HealthController {
 
