0d6ae00b080b57838b62aff758ed3fb5cc111678
qwebnm7788 <qwebnm7788@naver.com>
2021-05-16 19:04:57 +0900

    Separate kakao/naver service to fetch profile information

---
+++ src/main/java/com/basicbug/bikini/model/KakaoProfile.java
@@ -0,0 +1,30 @@
+package com.basicbug.bikini.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoProfile {
+
+    private Long id;
+    private Properties properties;
+
+    public String getId() {
+        return "kakao-" + id;
+    }
+
+    @Getter
+    @Setter
+    public static class Properties {
+        private String nickname;
+
+        public String getNickName() {
+            return nickname;
+        }
+    }
+}
