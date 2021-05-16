f3cf18b18e3e3530e94ffb70b3da36812bafa340
qwebnm7788 <qwebnm7788@naver.com>
2021-05-16 19:05:57 +0900

    Refactoring spring security implementation

--- src/main/java/com/basicbug/bikini/config/security/WebSecurityConfig.java
+++ src/main/java/com/basicbug/bikini/config/security/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.basicbug.bikini.config.security;
 
 import com.basicbug.bikini.filter.JwtAuthorizationFilter;
+import com.basicbug.bikini.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     @Override
@@ -19,18 +26,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+            .httpBasic().disable()
+            .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .cors()
             .and()
             .authorizeRequests()
             .antMatchers("/v1/auth/**").permitAll()
-            .antMatchers("/h2-console/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .headers().frameOptions().disable()
             .and()
-            .addFilter(new JwtAuthorizationFilter(authenticationManager()));
+            .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+            "/v2/api-docs", "/swagger-resources/**",
+            "/swagger-ui.html", "/webjars/**", "/swagger/**", "/h2-console/**", "/v1/auth/**");
     }
 }
--- src/main/java/com/basicbug/bikini/util/JwtTokenProvider.java
+++ src/main/java/com/basicbug/bikini/util/JwtTokenProvider.java
@@ -3,19 +3,34 @@ package com.basicbug.bikini.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.basicbug.bikini.model.UserPrincipal;
 import java.util.Date;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     private static final long EXPIRE_DURATION = 24 * 60 * 60 * 60 * 1000L;  // One day
 
-    public String generateToken(UserPrincipal userPrincipal) {
+    public String generateToken(String userPk, List<String> roles) {
         // TODO Need to fix!
+        // TODO secret key setup
+        Long currentTime = System.currentTimeMillis();
         return JWT.create()
-            .withSubject(userPrincipal.getUsername())
-            .withIssuedAt(new Date(System.currentTimeMillis()))
-            .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
+            .withClaim("roles", roles)
+            .withSubject(userPk)
+            .withIssuedAt(new Date(currentTime))
+            .withExpiresAt(new Date(currentTime + EXPIRE_DURATION))
             .sign(Algorithm.HMAC256("Basicbug"));
+    }
+
+    public boolean isValidToken(String token) {
+        return true;
+    }
+
+    private String getUserPk(String token) {
+        return JWT.decode(token).getSubject();
     }
 }
