1d219730f6001209cadfaf17541f4aa396619111
qwebnm7788 <qwebnm7788@naver.com>
2021-05-16 19:05:20 +0900

    Change user entity field

--- src/main/java/com/basicbug/bikini/model/User.java
+++ src/main/java/com/basicbug/bikini/model/User.java
@@ -1,31 +1,76 @@
 package com.basicbug.bikini.model;
 
+import com.basicbug.bikini.model.auth.AuthProvider;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import lombok.Data;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
 @Entity
-public class User extends BaseEntity {
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User extends BaseEntity implements UserDetails {
 
-    public User() {}
+    @Column(nullable = false, unique = true, length = 50)
+    private String uid;
 
-    public User(@Email @Size(max = 40) String email, @NotBlank @Size(max = 100) String password, @NotNull String userRole) {
-        this.email = email;
-        this.password = password;
-        this.userRole = userRole;
-    }
+    @Column(nullable = false, length = 100)
+    private String name;
 
-    @Email
-    @Size(max = 40)
-    private String email;
-
-    @NotBlank
-    @Size(max = 100)
+    @Column(length = 100)
     private String password;
 
-    private String userRole;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuthProvider provider;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return uid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
